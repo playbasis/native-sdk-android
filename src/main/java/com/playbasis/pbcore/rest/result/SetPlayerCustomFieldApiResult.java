e0403b090821f287a50b21a3295420d69c321685
Thaninrat Pipitcharulerd <tar@smartsoftasia.com>
2016-06-09 11:55:58 +0700

    Add SetPlayerCustomFieldApiResult

---
+++ src/main/java/com/playbasis/pbcore/rest/result/SetPlayerCustomFieldApiResult.java
@@ -0,0 +1,14 @@
+package com.playbasis.pbcore.rest.result;
+
+import java.util.List;
+
+/**
+ * Created by Tar on 4/28/16 AD.
+ */
+public class SetPlayerCustomFieldApiResult extends BaseUpdateUserApiResult<List<SetPlayerCustomFieldApiResult.Response>> {
+
+  public class Response {
+
+  }
+
+}
