c3da688543691875d0330309d8341a228bbbd3ad
Thaninrat Pipitcharulerd <tar@smartsoftasia.com>
2016-06-09 10:21:27 +0700

    Add PBApiResult.

---
+++ src/main/java/com/playbasis/pbcore/rest/result/PBApiResult.java
@@ -0,0 +1,25 @@
+package com.playbasis.pbcore.rest.result;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Tar on 4/21/16 AD.
+ */
+public abstract class PBApiResult<T> {
+
+  @SerializedName("success")
+  public Boolean success;
+  @SerializedName("error_code")
+  public String errorCode;
+  @SerializedName("message")
+  public String message;
+  @SerializedName("timestamp")
+  public Long timeStamp;
+  @SerializedName("time")
+  public String time;
+  @SerializedName("version")
+  public String version;
+  @SerializedName("response")
+  public T response;
+
+}
