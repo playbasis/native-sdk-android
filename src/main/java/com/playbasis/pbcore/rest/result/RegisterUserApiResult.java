126111b19b600e98c62dee6ed258895685f44449
Thaninrat Pipitcharulerd <tar@smartsoftasia.com>
2016-06-09 11:51:02 +0700

    Add RegisterUserApiResult

---
+++ src/main/java/com/playbasis/pbcore/rest/result/RegisterUserApiResult.java
@@ -0,0 +1,14 @@
+package com.playbasis.pbcore.rest.result;
+
+import java.util.List;
+
+/**
+ * Created by Tar on 4/21/16 AD.
+ */
+public class RegisterUserApiResult extends PBApiResult<List<RegisterUserApiResult.Response>> {
+
+  public class Response {
+
+  }
+
+}
