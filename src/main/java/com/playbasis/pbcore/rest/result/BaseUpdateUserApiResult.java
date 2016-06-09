7b79f2205f535e44cf9852a3b1846959808f50b0
Thaninrat Pipitcharulerd <tar@smartsoftasia.com>
2016-06-09 11:51:50 +0700

    Add BaseUpdateUserApiResult

---
+++ src/main/java/com/playbasis/pbcore/rest/result/BaseUpdateUserApiResult.java
@@ -0,0 +1,10 @@
+package com.playbasis.pbcore.rest.result;
+
+import com.playbasis.pbcore.rest.result.PBApiResult;
+
+/**
+ * Created by Tar on 4/28/16 AD.
+ */
+public abstract class BaseUpdateUserApiResult<T> extends PBApiResult<T> {
+
+}
