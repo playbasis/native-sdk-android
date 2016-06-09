33058a8eab059314c1a861b504f1a2dbc96b3bed
Thaninrat Pipitcharulerd <tar@smartsoftasia.com>
2016-06-09 12:03:53 +0700

    Add DeleteImageApiResult

---
+++ src/main/java/com/playbasis/pbcore/rest/result/DeleteImageApiResult.java
@@ -0,0 +1,11 @@
+package com.playbasis.pbcore.rest.result;
+
+/**
+ * Created by Tar on 4/21/16 AD.
+ */
+public class DeleteImageApiResult extends PBApiResult<DeleteImageApiResult.Response> {
+
+  public class Response {
+
+  }
+}
