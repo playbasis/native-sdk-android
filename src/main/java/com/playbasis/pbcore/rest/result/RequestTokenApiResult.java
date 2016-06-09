0182b2b065bbc2d5cad0a3190274924c8ebef5c7
Thaninrat Pipitcharulerd <tar@smartsoftasia.com>
2016-06-09 11:55:13 +0700

    Add RequestTokenApiResult

---
+++ src/main/java/com/playbasis/pbcore/rest/result/RequestTokenApiResult.java
@@ -0,0 +1,10 @@
+package com.playbasis.pbcore.rest.result;
+
+import com.playbasis.pbcore.rest.Token;
+
+/**
+ * Created by Tar on 4/21/16 AD.
+ */
+public class RequestTokenApiResult extends PBApiResult<Token> {
+
+}
