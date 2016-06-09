a936b695ed82d8a3eb323ed8a7d3e7bf3e92438d
Thaninrat Pipitcharulerd <tar@smartsoftasia.com>
2016-06-09 11:58:21 +0700

    Add UpdatePlayerOrganizationApiResult.

---
+++ src/main/java/com/playbasis/pbcore/rest/result/UpdatePlayerOrganizationApiResult.java
@@ -0,0 +1,12 @@
+package com.playbasis.pbcore.rest.result;
+
+/**
+ * Created by Tar on 4/28/16 AD.
+ */
+public class UpdatePlayerOrganizationApiResult extends BaseUpdateUserApiResult<UpdatePlayerOrganizationApiResult.Response> {
+
+  public class Response {
+
+  }
+
+}
