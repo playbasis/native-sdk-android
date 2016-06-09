233cddafff75f770e61bdadbe1b9c24a6a6b0659
Thaninrat Pipitcharulerd <tar@smartsoftasia.com>
2016-06-09 10:24:13 +0700

    Add PBApiErrorCheckFunc

---
+++ src/main/java/com/playbasis/pbcore/rest/result/function/PBApiErrorCheckFunc.java
@@ -0,0 +1,20 @@
+package com.playbasis.pbcore.rest.result.function;
+
+import com.playbasis.pbcore.rest.result.PBApiResult;
+import rx.exceptions.Exceptions;
+import rx.functions.Func1;
+
+/**
+ * Created by androiddev01 on 4/27/2016 AD.
+ */
+public class PBApiErrorCheckFunc<T extends PBApiResult> implements Func1<T,T> {
+  public static final String TAG = "PBApiErrorCheckFunc";
+
+  @Override
+  public T call(T t) {
+    if (!t.success){
+      throw Exceptions.propagate(new Throwable(t.message));
+    }
+    return t;
+  }
+}
