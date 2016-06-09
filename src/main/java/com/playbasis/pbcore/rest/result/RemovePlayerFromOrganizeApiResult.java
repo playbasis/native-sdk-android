9f414d975b7257cfa2c77bbc83a4258604079ccb
Thaninrat Pipitcharulerd <tar@smartsoftasia.com>
2016-06-09 11:54:13 +0700

    Add RemovePlayerFromOrganizeApiResult

---
+++ src/main/java/com/playbasis/pbcore/rest/result/RemovePlayerFromOrganizeApiResult.java
@@ -0,0 +1,12 @@
+package com.playbasis.pbcore.rest.result;
+
+/**
+ * Created by Tar on 4/28/16 AD.
+ */
+public class RemovePlayerFromOrganizeApiResult extends BaseUpdateUserApiResult<RemovePlayerFromOrganizeApiResult.Response> {
+
+  public class Response {
+
+  }
+
+}
