ef7da319c0c8cd2297630b716b305f7820daaa45
Thaninrat Pipitcharulerd <tar@smartsoftasia.com>
2016-06-09 10:00:43 +0700

    Create PBViewInterface.

---
+++ src/main/java/com/playbasis/pbcore/view/viewInterface/PBVIewInterface.java
@@ -0,0 +1,12 @@
+package com.playbasis.pbcore.view.viewInterface;
+
+/**
+ * Created by Tar on 6/9/16 AD.
+ */
+public interface PBVIewInterface extends com.smartsoftasia.ssalibrary.view.viewinterface.BaseViewInterface {
+
+  void displayProgress();
+
+  void hideProgress();
+
+}
