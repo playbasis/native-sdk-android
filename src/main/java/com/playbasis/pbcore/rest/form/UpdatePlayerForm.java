8ba74e2bfdf8ff03752cacb3dea60eea06747cd8
Thaninrat Pipitcharulerd <tar@smartsoftasia.com>
2016-06-09 12:35:13 +0700

    Create UpdatePlayerForm

--- src/main/java/com/playbasis/pbcore/rest/form/UpdatePlayerForm.java
+++ src/main/java/com/playbasis/pbcore/rest/form/UpdatePlayerForm.java
@@ -1,6 +1,11 @@
 package com.playbasis.pbcore.rest.form;
 
+import com.playbasis.pbcore.domain.model.Birthdate;
+import com.playbasis.pbcore.domain.model.Gender;
 import com.playbasis.pbcore.domain.model.Player;
+import com.smartsoftasia.ssalibrary.helper.Validator;
+
+import java.io.File;
 
 /**
  * Created by Tar on 6/9/16 AD.
@@ -8,5 +13,26 @@ import com.playbasis.pbcore.domain.model.Player;
 public class UpdatePlayerForm extends PBForm {
 
   public Player player;
+  public String playerId;
+  public String firstName;
+  public String lastName;
+  public Birthdate birthDate;
+  public Gender gender;
+  public File profilePictureFile;
+  public String imageUrl;
 
+  public UpdatePlayerForm(Player player, boolean preload) {
+    this.player = player;
+
+    if (preload) {
+      this.firstName = player.firstName;
+      this.lastName = player.lastName;
+      this.birthDate = player.birthday;
+      this.gender = player.gender;
+    }
+  }
+
+  public boolean isValidForm() {
+    return Validator.isValid(playerId);
+  }
 }
