fd41ab36ea1d9e43d4135c2c84be951f9bde33d0
Thaninrat Pipitcharulerd <tar@smartsoftasia.com>
2016-06-09 15:52:49 +0700

    Add StoreOrganizeService

--- src/main/java/com/playbasis/pbcore/rest/result/StoreOrganizeApiResult.java
+++ src/main/java/com/playbasis/pbcore/rest/result/StoreOrganizeApiResult.java
@@ -8,16 +8,16 @@ import java.util.List;
 /**
  * Created by Tar on 4/26/16 AD.
  */
-public class OrganizationApiResult<T extends Organization> extends PBApiResult<OrganizationApiResult.Response> {
+public class StoreOrganizeApiResult extends PBApiResult<StoreOrganizeApiResult.Response> {
 
-  public List<T> getOrganizations() {
+  public List<Organization> getOrganizations() {
     return response.organizations;
   }
 
   public class Response {
 
     @SerializedName("results")
-    public List<T> organizations;
+    public List<Organization> organizations;
   }
 
 }
---
+++ src/main/java/com/playbasis/pbcore/rest/service/StoreOrganizeService.java
@@ -0,0 +1,19 @@
+package com.playbasis.pbcore.rest.service;
+
+import android.support.annotation.NonNull;
+
+import com.playbasis.pbcore.rest.result.StoreOrganizeApiResult;
+
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import rx.Observable;
+
+public interface StoreOrganizeService {
+
+  @GET("StoreOrg/nodes")
+  Observable<StoreOrganizeApiResult> getStoreOrganize(
+    @NonNull @Query("api_key") String apiKey,
+    @Query("organize_id") String organizeId
+  );
+
+}
