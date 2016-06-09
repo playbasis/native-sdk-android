1bdf1d9af95c0d6a6fb9071445e3dd534d6acd2b
Thaninrat Pipitcharulerd <tar@smartsoftasia.com>
2016-06-09 11:26:23 +0700

    Add constructor to Gender

--- src/main/java/com/playbasis/pbcore/domain/model/Gender.java
+++ src/main/java/com/playbasis/pbcore/domain/model/Gender.java
@@ -3,8 +3,6 @@ package com.playbasis.pbcore.domain.model;
 import com.playbasis.pbcore.R;
 import com.smartsoftasia.ssalibrary.helper.TranslationHelper;
 
-import java.util.ArrayList;
-
 /**
  * Created by Tar on 6/9/16 AD.
  */
@@ -13,34 +11,25 @@ public class Gender extends PBModel {
   public static final String MALE = "1";
   public static final String FEMALE = "2";
 
-  public static ArrayList<Gender> genders = null;
-
   public String title;
   public String value;
-
-  public static ArrayList<Gender> all() {
-    if (genders == null) {
-      genders = new ArrayList<>();
-      genders.add(new Gender(TranslationHelper.get(R.string.global_male), MALE));
-      genders.add(new Gender(TranslationHelper.get(R.string.global_female), FEMALE));
-    }
-
-    return genders;
-  }
-
-  public static Gender find(int value) {
-    for (Gender gender : all()) {
-      if (gender.value == String.valueOf(value)) {
-        return gender;
-      }
-    }
-
-    return null;
-  }
 
   public Gender(String title, String value) {
     this.title = title;
     this.value = value;
   }
 
+  public Gender(String value) {
+    this.value = value;
+
+    if (value.equalsIgnoreCase(MALE)) {
+      this.title = TranslationHelper.get(R.string.global_male);
+    } else if (value.equalsIgnoreCase(FEMALE)) {
+      this.title = TranslationHelper.get(R.string.global_female);
+    }
+  }
+
+  public Gender(int value) {
+    this(String.valueOf(value));
+  }
 }
