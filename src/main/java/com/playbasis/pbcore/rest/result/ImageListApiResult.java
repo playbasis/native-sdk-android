f7e12c56a70e8b18d9a239bb6bc44dec6c040b4d
Thaninrat Pipitcharulerd <tar@smartsoftasia.com>
2016-06-09 12:05:37 +0700

    Add ImageListApiResult

---
+++ src/main/java/com/playbasis/pbcore/rest/result/ImageListApiResult.java
@@ -0,0 +1,21 @@
+package com.playbasis.pbcore.rest.result;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Tar on 4/21/16 AD.
+ */
+public class ImageListApiResult extends PBApiResult<List<ImageListApiResult.Response>> {
+
+  public class Response {
+    @SerializedName("file_name")
+    public String fileName;
+    @SerializedName("date_added")
+    public Date dateAdded;
+    @SerializedName("date_modified")
+    public Date dateModified;
+  }
+}
