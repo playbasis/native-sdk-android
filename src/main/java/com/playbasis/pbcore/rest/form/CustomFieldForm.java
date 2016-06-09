821cdce4c18a35417313ab28c11692d4cf091291
Thaninrat Pipitcharulerd <tar@smartsoftasia.com>
2016-06-09 12:18:00 +0700

    Add CustomFieldForm

---
+++ src/main/java/com/playbasis/pbcore/rest/form/CustomFieldForm.java
@@ -0,0 +1,57 @@
+package com.playbasis.pbcore.rest.form;
+
+import android.text.TextUtils;
+
+import com.playbasis.pbcore.rest.form.PBForm;
+import com.smartsoftasia.ssalibrary.helper.Validator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by Tar on 5/27/16 AD.
+ */
+public class CustomFieldForm extends PBForm {
+
+  protected HashMap<String, String> customFieldMap = new HashMap<>();
+
+  public String getKeys() {
+    ArrayList<String> keys = new ArrayList<>();
+
+    if (customFieldMap != null) {
+      for (String key : customFieldMap.keySet()) {
+
+        if (customFieldMap.get(key) != null) {
+          keys.add(key);
+        }
+      }
+    }
+
+    return TextUtils.join(",", keys);
+  }
+
+  public String getValues() {
+    ArrayList<String> values = new ArrayList<>();
+
+    if (customFieldMap != null) {
+      for (String key : customFieldMap.keySet()) {
+        String value = customFieldMap.get(key);
+
+        if (value != null) {
+          values.add(value);
+        }
+      }
+    }
+    return TextUtils.join(",", values);
+  }
+
+  public void setCustomField(String key, String value) {
+    if (Validator.isValid(key) && value != null) {
+      customFieldMap.put(key, value);
+    }
+  }
+
+  public void removeCustomField(String key) {
+    customFieldMap.remove(key);
+  }
+}
