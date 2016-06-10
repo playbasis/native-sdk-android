05e8a97354b9f444ad635638f5cbce62193e5f27
Thaninrat Pipitcharulerd <tar@smartsoftasia.com>
2016-06-10 16:23:33 +0700

    Add ContentOpinionInteractor, LikeContentInteractor, DislikContentInteractor, ContentFeedbackInteractor

---
+++ src/main/java/com/playbasis/pbcore/rest/form/ContentFeedbackForm.java
@@ -0,0 +1,43 @@
+package com.playbasis.pbcore.rest.form;
+
+
+import com.playbasis.pbcore.domain.model.Content;
+import com.playbasis.pbcore.domain.model.Player;
+
+/**
+ * Created by Tar on 5/26/16 AD.
+ */
+public class ContentFeedbackForm extends ContentOpinionForm {
+
+  protected String comment = null;
+  protected String key = null;
+  protected String value = null;
+
+  public ContentFeedbackForm(Content content, Player player) {
+    super(content, player);
+  }
+
+  public String getComment() {
+    return comment;
+  }
+
+  public void setComment(String comment) {
+    this.comment = comment;
+  }
+
+  public String getKey() {
+    return key;
+  }
+
+  public void setKey(String key) {
+    this.key = key;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  public void setValue(String value) {
+    this.value = value;
+  }
+}
