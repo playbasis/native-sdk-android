66b0cb02fca4ee3e156320889dd56cb059bcf721
Thaninrat Pipitcharulerd <tar@smartsoftasia.com>
2016-06-13 11:07:41 +0700

    Add PBAppConfig and PBApplication

---
+++ src/main/java/com/playbasis/pbcore/core/PBAppConfig.java
@@ -0,0 +1,13 @@
+package com.playbasis.pbcore.core;
+
+/**
+ * Created by Tar on 6/13/16 AD.
+ */
+public class PBAppConfig {
+
+  public static final String BASE_URL = "https://api.pbapp.net/";
+
+  public static final int PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 102;
+  public static final int PERMISSIONS_REQUEST_CAMERA = 103;
+
+}
---
+++ src/main/java/com/playbasis/pbcore/core/PBApplication.java
@@ -0,0 +1,10 @@
+package com.playbasis.pbcore.core;
+
+import com.smartsoftasia.ssalibrary.core.BaseApplication;
+
+/**
+ * Created by Tar on 6/13/16 AD.
+ */
+public abstract class PBApplication extends BaseApplication {
+
+}
