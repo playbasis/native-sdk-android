d25c58075d4fb0969a03b559c86635921953c4d9
Thaninrat Pipitcharulerd <tar@smartsoftasia.com>
2016-08-30 14:51:31 +0700

    Correct response of VerifyOTPCodeApiResult

--- src/main/java/com/playbasis/pbcore/rest/result/player/VerifyOTPCodeApiResult.java
+++ src/main/java/com/playbasis/pbcore/rest/result/player/VerifyOTPCodeApiResult.java
@@ -2,10 +2,12 @@ package com.playbasis.pbcore.rest.result.player;
 
 import com.playbasis.pbcore.rest.result.PBApiResult;
 
+import java.util.List;
+
 /**
  * Created by Tar on 4/28/16 AD.
  */
-public class VerifyOTPCodeApiResult extends PBApiResult<VerifyOTPCodeApiResult.Response> {
+public class VerifyOTPCodeApiResult extends PBApiResult<List<VerifyOTPCodeApiResult.Response>> {
 
   public class Response {
 
