2aad0be2d020809063855d3afde845e85134d5d6
Thaninrat Pipitcharulerd <tar@smartsoftasia.com>
2016-09-14 15:13:53 +0700

    Prevent null pointer exception in EventAdapter

--- src/main/java/com/playbasis/pbcore/rest/adapter/EventAdapter.java
+++ src/main/java/com/playbasis/pbcore/rest/adapter/EventAdapter.java
@@ -13,20 +13,33 @@ import java.lang.reflect.Type;
 /**
  * Created by Tar on 6/29/16 AD.
  */
-public class EventAdapter implements JsonDeserializer<EventResponse>  {
+public class EventAdapter implements JsonDeserializer<EventResponse> {
 
   @Override
   public EventResponse deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+
+    EventResponse eventResponse = new EventResponse();
+    if (json == null) {
+      return eventResponse;
+    }
 
     JsonObject jsonObject = (JsonObject) json;
     if (jsonObject.has("value")) {
       jsonObject.addProperty("reward_value", jsonObject.get("value").getAsString());
     }
 
-    EventResponse eventResponse = new EventResponse();
-    eventResponse.eventType = jsonObject.get("event_type").getAsString();
-    eventResponse.logId = jsonObject.get("log_id").getAsString();
-    eventResponse.index = jsonObject.get("index").getAsInt();
+    if (jsonObject.has("event_type")) {
+      eventResponse.eventType = jsonObject.get("event_type").getAsString();
+    }
+
+    if (jsonObject.has("log_id")) {
+      eventResponse.logId = jsonObject.get("log_id").getAsString();
+    }
+
+    if (jsonObject.has("index")) {
+      eventResponse.index = jsonObject.get("index").getAsInt();
+    }
+
     eventResponse.rewardResponse = context.deserialize(jsonObject, RewardResponse.class);
 
     return eventResponse;
