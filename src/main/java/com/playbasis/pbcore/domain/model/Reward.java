7db59d103f5cff87946bacba8f6a38a09de70dbc
Thaninrat Pipitcharulerd <tar@smartsoftasia.com>
2016-09-20 12:56:52 +0700

    Remove Parcelable implementation from Reward

--- src/main/java/com/playbasis/pbcore/domain/model/Reward.java
+++ src/main/java/com/playbasis/pbcore/domain/model/Reward.java
@@ -12,7 +12,7 @@ import java.util.List;
 /**
  * Created by Tar on 6/22/16 AD.
  */
-public class Reward extends PBModel implements Parcelable {
+public class Reward extends PBModel {
 
   protected String rewardId;
   protected String type;
