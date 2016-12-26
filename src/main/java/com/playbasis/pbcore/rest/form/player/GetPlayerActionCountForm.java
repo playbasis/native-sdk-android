a12e0c0babffe9695f473e881e3f7e7021dce6a5
Nott <nawamin@smartsoftasia.com>
2016-12-26 11:39:56 +0700

    Add custom params for PlayerAPI Action count

--- src/main/java/com/playbasis/pbcore/rest/form/player/GetPlayerActionCountForm.java
+++ src/main/java/com/playbasis/pbcore/rest/form/player/GetPlayerActionCountForm.java
@@ -1,11 +1,11 @@
 package com.playbasis.pbcore.rest.form.player;
 
-import com.playbasis.pbcore.rest.form.PBForm;
+import com.playbasis.pbcore.rest.form.CustomParamsForm;
 
 /**
  * Created by Tar on 6/10/16 AD.
  */
-public class GetPlayerActionCountForm extends PBForm {
+public class GetPlayerActionCountForm extends CustomParamsForm {
 
   public String playerId;
   public String actionName;
